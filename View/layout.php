412b42e735ef0ea9986f34bd016fbe2491d94b9c
Sergio <marques.sergio96@gmail.com>
2018-04-24 23:12:56 +0200

    Pantalla Inicio

--- View/layout.php
+++ View/layout.php
@@ -10,7 +10,7 @@ if (isset($_GET['pagina'])) {
 ?>
 <html>
 <head>
-    <link rel="stylesheet" href="Webroot/css/estilosLogin.css" type="text/css">
+    <link rel="stylesheet" href="Webroot/css/estilosInicio.css" type="text/css">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
@@ -19,32 +19,14 @@ if (isset($_GET['pagina'])) {
 </head>
 <body>
 <?php require_once $vista; ?>
-<nav>
-    <a href="index.php?pagina=inicio"">INICIO</a>
-    <a href="index.php?pagina=carta">CARTA</a>
-    <a href="index.php?pagina=reservas">RESERVAS</a>
-    <a href="index.php?pagina=contacto">CONTACTO</a>
-
-
-</nav>
-</body>
-<footer style="position: fixed;
+<footer style="position: relative;
     width: 100%;
-    bottom: 0px;">
-    <div id="pie">
-        <span style="float: left;">Web de Sergio Marqués</span>
-        <a class="btn btn-link" href="index.php?pagina=codigo">Codigo</a>
-        <a class="btn btn-link"
-           href="http://daw-usgit.sauces.local/SMM_1718/dwes/tree/master/ProyectoTema7/LoginLogoff">Repositorio</a>
-        <a class="btn btn-link" href="Documentación/phpdoc/index.html">PHPDoc</a>
-        <a class="btn btn-link" href="index.php?pagina=tecnologias">Tecnologias</a>
-        <a class="btn btn-link"
-           href="<?php if (isset($_GET['pagina']) && $_GET['pagina'] != "inicio" && $_GET['pagina'] != "login") {
-               echo "index.php?pagina=WIP&anterior=" . $_GET['pagina'];
-           } else {
-               echo " index.php?pagina=WIP ";
-           } ?>">RSS</a>
-
-    </div>
+    height:70px;
+    background-color: white;
+    bottom: 0px;text-align: center">
+    <h3 >DERECHOS RESERVADOS MARQUÉS COMPANY S.L.</h3><a style="float:right"href="#"><span class="
+glyphicon glyphicon-arrow-up">ir arriba</span> </a>
 </footer>
+</body>
+
 </html>
--- View/vinicio.php
+++ View/vinicio.php
@@ -1,3 +1,36 @@
-<?php
-echo "INICIO";
-?>
+
+<nav style="width:100%; height:40px;background-color: black;">
+    <a href="#" style="color:white;float:right;margin-right: 10px;margin-top:10px;"><span  class="glyphicon glyphicon-log-in"></span> Iniciar sesión</a>
+</nav>
+<div style="text-align: center">
+<img src="../Webroot/media/fondo-restaurante.jpg" style="width: 100%;height:80%;display: block;
+     position: relative;"/>
+
+    <h1 id="tituloInicio1" >PROYECTO DAW</h1>
+    <h2 id="tituloInicio2">ESPECIALIDAD EN ALGO</h2>
+
+</div>
+
+<nav class="navbar navbar-inverse" style="height: auto;background-color: #0F0F0F">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" id="tituloNav"><u>PROYECTO DAW</u></a>
+        </div>
+        <ul class="nav navbar-nav" style="float:right;margin-right: 40px;height:75px;">
+            <li class="botonesInicio"><a href="index.php?pagina=inicio"">INICIO</a></li>
+            <li class="botonesInicio">    <a href="index.php?pagina=carta">CARTA</a>
+            </li>
+            <li class="botonesInicio">    <a href="index.php?pagina=reservas">RESERVAS</a>
+            </li>
+            <li class="botonesInicio">    <a href="index.php?pagina=contacto">CONTACTO</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div style="margin:0 auto;width:100%; height: auto;text-align: center;margin-top:50px;">
+    <div style="display:inline-flex">
+        <div><img class="imagenesMuestra" src="../Webroot/media/cocina.jpg" /><p class="pieDeImagen">Los mejores chefs del país.</p></div>
+        <div><img class="imagenesMuestra" src="../Webroot/media/barra.jpg" /><p class="pieDeImagen">Disponemos de barra con cocteles.</p></div>
+        <div><img class="imagenesMuestra" src="../Webroot/media/vistas.jpg" /><p class="pieDeImagen">Vistas alucinantes.</p></div>
+    </div>
+</div>
