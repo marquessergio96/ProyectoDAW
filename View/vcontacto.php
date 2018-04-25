752bb2ee2e779d33b4df52b021f5d63dcfd892df
Sergio <marques.sergio96@gmail.com>
2018-04-25 22:24:30 +0200

    Zona contacto con iframe de google maps

--- View/vcontacto.php
+++ View/vcontacto.php
@@ -1,3 +1,30 @@
-<?php
-echo "CONTACTO";
-?>
+<nav class="navbar navbar-inverse" style="position: fixed;
+  top: 0;
+  width: 100%;  opacity: 0.9;
+">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#" id="tituloNav"><u>PROYECTO DAW</u></a>
+        </div>
+        <ul class="nav navbar-nav" style="float:right;margin-right: 40px;height:75px;">
+            <li class="botonesInicio"><a href="index.php?pagina=inicio"">INICIO</a></li>
+            <li class="botonesInicio">    <a href="index.php?pagina=carta">CARTA</a>
+            </li>
+            <li class="botonesInicio">    <a href="index.php?pagina=reservas">RESERVAS</a>
+            </li>
+            <li class="botonesInicio">    <a href="index.php?pagina=contacto">CONTACTO</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<h1 style="text-align: center;margin-top: 100px;">CONTÁCTENOS</h1>
+<div style="width: 85%; margin-left:15%;margin-top:50px;display: inline-flex;">
+    <iframe  width="45%" height="60%" src="https://maps.google.com/maps?width=100%&height=600&hl=es&q=c%2Fmedul%20%2CLe%C3%B3n+(Proyecto%20DAW)&ie=UTF8&t=p&z=16&iwloc=B&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+    </iframe>
+    <div style="margin-left: 40px;">
+    <div class="contactos"><h1 class="titulosContactos"><span class="glyphicon glyphicon-map-marker"></span> Proyecto DAW</h1><h3 id="calle">C/ Medul nº 22,León</h3></div>
+    <div class="contactos"><h1 class="titulosContactos"><span class="glyphicon glyphicon-phone"></span> (+34) 696 77 06 92</h1></div>
+    <div class="contactos"><h1 class="titulosContactos"><span class="glyphicon glyphicon-envelope"></span> sergio@proyectodaw.com </h1></div>
+    </div>
+</div><br />
+<div style="height: 200px;"></div>
